9b5f14a975f25aac5997231170b7a3efb0efb264
Hannes Widrig <hanneswidrig@gmail.com>
2026-02-27 08:13:06 -0500

    chore: fix @lib resolution

--- apps/api/tsdown.config.ts
+++ apps/api/tsdown.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "tsdown";
 
-export default defineConfig({
-  entry: ["src/index.ts"]
-});
+export default defineConfig((cfg) => ({
+  entry: ["src/index.ts"],
+  onSuccess: cfg.watch ? "node apps/api/dist/index.mjs" : undefined,
+}));
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   workspace: ["apps/*", "libs/*"],
   entry: ["src/index.ts"],
+  dts: {
+    tsgo: true,
+  },
   exports: {
     devExports: "dev",
   },
